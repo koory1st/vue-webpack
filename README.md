b5b9107dd748acc4e8f3d23c57ae9a4cc6a8f2b3
koory1st <32436334@qq.com>
2019-05-11 22:02:34 +0800

    配置devserver

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://www.imooc.com/learn/935
 
 # 安装的包
 npm i webpack vue vue-loader css-loader vue-template-compiler webpack-cli url-loader style-loader file-loader stylus-loader stylus
+npm i webpack-dev-server
+npm i cross-env
+npm i html-webpack-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,16 @@ const path = require('path')
 // vue - loader was used without the corresponding plugin.Make sure to include VueLoaderPlugin in your webpack config.
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
+// 用来生成html
+const HTMLPlugin = require('html-webpack-plugin')
+
+const webpack = require('webpack')
+
+// 根据打包时的参数判断是否是开发环境
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: "web",
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -33,12 +42,41 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 1024,
-          name: '[name].[ext]'
+          name: '[name]-aaa.[ext]'
         }
       }]
     }]
   },
   plugins: [
     new VueLoaderPlugin(),
+    // 用来在生成的html里添加环境变量
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
   ]
 }
+
+if (isDev) {
+  // 添加dev-server的配置
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0', // 同时兼容localhost及内网ip
+    overlay: {
+      errors: true // 在页面上直接显示错误
+    },
+    open: true,
+    hot: true
+  }
+
+  config.devtool = '#cheap-module-eval-source-map'
+
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+}
+
+module.exports = config
